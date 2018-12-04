49ed9383ba0371a8c053f88bad21b2a032a4dd67
pikachu <hooraypikachu@gmail.com>
2018-12-04 22:18:40 +0800

    import curl & json; test OK

---
+++ BeautyCam/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "facepp.h"
+
+const char * filePath = "0171_01.jpg";
+
+int nin(int argc, const char * argv[]) {
+	const char * key = "x-pq6flagmculJoKDhhuCDSIkWARj-jG";
+	const char * secret = "UbpURpZq7jWRDDwk0K8gQPVZYuk6LT9t";
+	return 0;
+}
